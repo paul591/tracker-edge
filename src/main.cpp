e6a6adf476706ec0112a287652a81621529c0b4a
Eric Berseth <eric.berseth@particle.io>
2022-01-29 19:02:32 -0800

    Provide simple application

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Particle Industries, Inc.
+ * Copyright (c) 2022 Particle Industries, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,9 @@
 #include "tracker_config.h"
 #include "tracker.h"
 
+#include "DebounceSwitchRK.h"
+#include "ClosedCube_SHT31D.h"
+
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
@@ -29,19 +32,90 @@ STARTUP(
     Tracker::startup();
 );
 
-SerialLogHandler logHandler(115200, LOG_LEVEL_TRACE, {
-    { "app.gps.nmea", LOG_LEVEL_INFO },
-    { "app.gps.ubx",  LOG_LEVEL_INFO },
-    { "ncp.at", LOG_LEVEL_INFO },
-    { "net.ppp.client", LOG_LEVEL_INFO },
+Logger appLogger("app.main");
+
+SerialLogHandler logHandler(115200, LOG_LEVEL_NONE, {
+    { "app.main", LOG_LEVEL_INFO },
 });
 
-void setup()
-{
-    Tracker::instance().init();
+
+SHT31D_CC::ClosedCube_SHT31D sts31;
+
+static SHT31D_CC::SHT31D_ErrorCode startSts31() {
+    auto ret = sts31.periodicStart(SHT31D_CC::REPEATABILITY_MEDIUM, SHT31D_CC::FREQUENCY_HZ5);
+    if (SHT31D_CC::SHT31D_ErrorCode::NO_ERROR != ret) {
+        appLogger.error("STS31 sensor setup failure");
+    }
+
+    return ret;
 }
 
-void loop()
-{
+static SHT31D_CC::SHT31D_ErrorCode stopSts31() {
+    return sts31.periodicStop();
+}
+
+void appSleep(TrackerSleepContext context) {
+    // Put STS31 into idle mode
+    stopSts31();
+}
+
+void appWake(TrackerSleepContext context) {
+    // Put STS31 into periodic sampling
+    startSts31();
+}
+
+int appPublish(JSONWriter& message, LocationPoint& point, const void* context) {
+    auto temp = sts31.periodicFetchData();
+    if (SHT31D_CC::SHT31D_ErrorCode::NO_ERROR != temp.error) {
+        message.name("c_temp").value((double)temp.t, 1);
+    }
+    return SYSTEM_ERROR_NONE;
+}
+
+
+void setup() {
+    // Optional - Wait for a waiting terminal for showing logs
+    waitFor(Serial.isConnected, 10000);
+
+    Tracker::instance().init();
+
+    // Associate user button with debounce handler
+    // See https://github.com/rickkas7/DebounceSwitchRK for more information
+    // Some example DebouncePressState states:
+    //           Single tap (< 3s):  PRESS_START -> SHORT -> RELEASED -> TAP
+    //           Double tap (< 3s):  PRESS_START -> SHORT -> RELEASED -> PRESS_START -> SHORT -> RELEASED -> TAP
+    //    Long press (> 3s, < 10s):  PRESS_START -> PROGRESS -> LONG -> RELEASED
+    //    Very long press  (> 10s):  PRESS_START -> PROGRESS -> VERY_LONG -> RELEASED
+    DebounceSwitch::getInstance()->setup();
+    DebounceSwitch::getInstance()->addSwitch(TRACKER_89503_USER_BUTTON, DebounceSwitchStyle::PRESS_LOW_PULLUP,
+        [](DebounceSwitchState* switchState, void *) {
+            appLogger.info("toggle state=%s", switchState->getPressStateName());
+        });
+
+    // Configure STS3X temperature (+humidity) sensor
+    sts31.begin(TRACKER_89503_STS3X_I2C_ADDR);  // No real error path
+    startSts31();
+
+    // Register for Tracker events
+    TrackerSleep::instance().registerSleep(appSleep);
+    TrackerSleep::instance().registerWake(appWake);
+    TrackerLocation::instance().regLocGenCallback(appPublish);
+
+    // We want the user button to wake us up
+    TrackerSleep::instance().wakeFor(TRACKER_89503_USER_BUTTON, FALLING);
+}
+
+static unsigned int tick {};
+
+void loop() {
     Tracker::instance().loop();
+
+    if ((System.uptime() - tick) > 10) {
+        tick = System.uptime();
+        // Throw away the humidity measurement because the device is an STS31
+        auto temp = sts31.periodicFetchData();
+        if (SHT31D_CC::SHT31D_ErrorCode::NO_ERROR != temp.error) {
+            appLogger.info("Temperature = %0.1f", temp.t);
+        }
+    }
 }
